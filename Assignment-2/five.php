f72f541df76b62c0a21f4ad864ac156621212f38
alkeshrohit22 <alkesh.rohit@sigmainfo.net>
2023-02-24 12:03:23 +0530

    In Assignmnet 2 fifth program changed

--- Assignment-2/five.php
+++ Assignment-2/five.php
@@ -1,7 +1,12 @@
 <?php
 
 echo "All the numbers between 50 and 150 that are divisible by 4 <br />";
-$arr = range(50, 150, 4);
-echo implode(", ", $arr);
+$arr = range(50, 150);
+
+foreach($arr as $v){
+    if($v%4 == 0){
+        echo $v.", ";
+    }
+}
 
 ?>
