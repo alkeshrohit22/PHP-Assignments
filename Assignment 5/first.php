fc2110efcade28684c6f2904b9bc377d73e512a6
alkeshrohit22 <alkesh.rohit@sigmainfo.net>
2023-02-28 16:36:54 +0530

    update string reverse function

--- Assignment 5/first.php
+++ Assignment 5/first.php
@@ -4,7 +4,13 @@
 
 //reverse string function
 function get_rev_string($text){
-    return strrev($text);
+    for($i=strlen($text)-1, $j=0; $j<$i; $i--, $j++) 
+    {
+        $temp = $text[$i];
+        $text[$i] = $text[$j];
+        $text[$j] = $temp;
+    }
+    return $text;
 }
 
 $text = 'Alkesh Rohit';
@@ -12,5 +18,7 @@ echo "Original String : ".$text ."<br />";
 echo "<br />";
 echo "Reverse String : ";
 echo get_rev_string($text);
+
+
 
 //EOF
