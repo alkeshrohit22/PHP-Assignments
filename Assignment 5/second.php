1d9a84316ce63608b50c48146de49684065a6edc
alkeshrohit22 <alkesh.rohit@sigmainfo.net>
2023-02-28 15:06:40 +0530

    Update sort function

--- Assignment 5/second.php
+++ Assignment 5/second.php
@@ -2,14 +2,60 @@
 
 //2. Write a function to sort an array.
 
-//function to sort an array
-function sort_array($arr){
-    sort($arr);
-    echo "Sorted Array : ";
-    display_arr($arr);
+//function to sort an array 
+function mergesort(&$arr, $left, $right)
+{
+    if ($left < $right) {
+        $mid = $left + (int) (($right - $left) / 2);
+        mergesort($arr, $left, $mid);
+        mergesort($arr, $mid + 1, $right);
+        merge($arr, $left, $mid, $right);
+    }
 }
 
-//Display function
+// for merging array function
+function merge(&$arr, $left, $mid, $right)
+{
+    //create to temporary array temp1 and temp2
+    $temp1 = $mid - $left + 1;
+    $temp2 = $right - $mid;
+    $leftArr = array_fill(0, $temp1, 0);
+    $rightArr = array_fill(0, $temp2, 0);
+    for ($i = 0; $i < $temp1; $i++) {
+        $leftArr[$i] = $arr[$left + $i];
+    }
+    for ($i = 0; $i < $temp2; $i++) {
+        $rightArr[$i] = $arr[$mid + $i + 1];
+    }
+
+    $x = 0;
+    $y = 0;
+    $z = $left;
+    while ($x < $temp1 && $y < $temp2) {
+        if ($leftArr[$x] < $rightArr[$y]) {
+            $arr[$z] = $leftArr[$x];
+            $x++;
+        } else {
+            $arr[$z] = $rightArr[$y];
+            $y++;
+        }
+        $z++;
+    }
+
+    while ($x < $temp1) {
+        $arr[$z] = $leftArr[$x];
+        $x++;
+        $z++;
+    }
+
+    while ($y < $temp2) {
+        $arr[$z] = $rightArr[$y];
+        $y++;
+        $z++;
+    }
+}
+
+//display function
 function display_arr($number){
     foreach($number as $n){
         echo $n." ";
@@ -17,12 +63,15 @@ function display_arr($number){
 }
 
 
-$number = array(1, 5, 9, 3, 7, 4);
+$number = array(1, 5, 9, 3, 7, 4, -4, -3, -5);
 echo "Original Array : ";
 display_arr($number);
 echo "<br />";
 
 //calling function
-sort_array($number);
+mergesort($number, 0, sizeof($number) - 1);
+echo "Sorted Array : ";
+display_arr($number);
+
 
 //EOF
