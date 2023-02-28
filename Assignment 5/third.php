31a11a22fb367b86646cdd63406da184f0cb3302
alkeshrohit22 <alkesh.rohit@sigmainfo.net>
2023-02-28 17:33:11 +0530

    palindrome function change

--- Assignment 5/third.php
+++ Assignment 5/third.php
@@ -2,22 +2,31 @@
 
 //3. Write a PHP function that checks whether a passed string is a palindrome or not?
 
+//function to reverse string
+function reverseString($text)
+{
+    for ($i = strlen($text) - 1, $j = 0; $j < $i; $i--, $j++) {
+        $temp = $text[$i];
+        $text[$i] = $text[$j];
+        $text[$j] = $temp;
+    }
+    return $text;
+}
+
 //Function check palindrome string
-function check_palindrome($text){
-    if($text == strrev($text)){
-        return 1;
+function check_palindrome($orn, $rev)
+{
+    if ($orn == $rev) {
+        echo "String is palindrome!!!";
+    } else {
+        echo "String is not palindrome!!!";
     }
 }
 
-$string1 = 'Hey! My name is Alkesh Rohit.';
-$value = check_palindrome($string1);
+$original_str = 'Hey! My name is Alkesh';
+$rev_str = reverseString($original_str);
 
-//Display Output
-if($value == 1){
-    echo "String is palindrome!!!";
-}else{
-    echo "String is not-palindrome!!!";
-}
-
+//caling palindrome checking
+check_palindrome($original_str, $rev_str);
 
 //EOF
